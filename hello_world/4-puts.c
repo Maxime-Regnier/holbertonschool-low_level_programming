10579666513987b9871859fb82f39f42bbb48044
Maxime-Regnier <11493@holbertonstudents.com>
2025-06-16 13:51:48 +0000

    file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,17 @@
 #!/bin/bash
-puts"Programming is like building a multilingual puzzle
+#include <stdio.h>
+
+
+/**
+ * main - printf 'Programming is like building a multilingual
+ * puzzle, followed by a newline.
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+        puts("\"Programming is like building a multilingual puzzle");
+
+        return (0);
+}
