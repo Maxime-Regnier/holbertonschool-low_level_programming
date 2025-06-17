252359cbeca4616f182db2a4ef65693de0bfbfe2
Maxime-Regnier <11493@holbertonstudents.com>
2025-06-17 09:44:29 +0000

    file

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 
-void checkNum(int N) {
-
-    // Check if the number is zero
-    if (N == 0) {
-        printf("Zero\n");
-        return;
-    }
-
-    // Extracting msb
-    int msb = N & (1 << (sizeof(int) * 8 - 1));
-
-    if (msb)
+int main()
+{
+    int n;
+ 
+    scanf("%d",&n);
+ 
+    if(n == 0)
+        printf("Neither positive nor negative");
+    else if(n < 0)
         printf("Negative\n");
     else
         printf("Positive\n");
-}
-
-int main() {
-    int N = 10;
-    checkNum(N);
+     
     return 0;
 }
