40768ce727922aef2a468ae4079ee3aaa1774944
Maxime-Regnier <11493@holbertonstudents.com>
2025-06-19 16:02:24 +0000

    Test

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+int i = 'a';
+int j = 'A';
+
+while ( i <= 'z') {
+	putchar (i);
+	i++;
+}
+while ( i <= 'Z') {
+	putchar (i);
+	i++;
+
+return (0);
+}
