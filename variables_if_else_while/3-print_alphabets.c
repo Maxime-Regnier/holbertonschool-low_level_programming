7565d0af2bf7da56902bfea07ccf528672890906
Maxime-Regnier <11493@holbertonstudents.com>
2025-06-19 16:40:56 +0000

    Test

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 int i = 'a';
 int j = 'A';
 
-while ( i <= 'z') {
-	putchar (i);
-	i++;
-}
-while ( i <= 'Z') {
+while (i <= 'z')
+{
 	putchar (i);
 	i++;
 
+}
+
+while (i <= 'Z')
+{
+	putchar (i);
+	i++;
+}
 return (0);
+
 }
