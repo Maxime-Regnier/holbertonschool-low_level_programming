b53e27363a28ad881b37417b14cc0b09ab4f36ec
Maxime-Regnier <11493@holbertonstudents.com>
2025-06-20 08:52:50 +0000

    modifier

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 0; x < 10; x++)
+	{
+	putchar(x + '0');
+	}
+
+	putchar('\n');
+	return (0);
+
+}
