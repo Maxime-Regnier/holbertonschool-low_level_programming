f5e0b2dfff553b9938e760919562714c3280c0d5
Maxime-Regnier <11493@holbertonstudents.com>
2025-06-23 14:13:40 +0000

    file

---
+++ more_functions_nested_loops/2-mul.C
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *
+ *
+ *
+ *
+
+
+int mul(int a, int b);
+{
+
+	int somme = 98 * 1024;
+
+	printf("98 * 1024 = %d\n", somme);
+	return (0);
+}
---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * mul - Multiplie deux entiers.
+ * @a: 98
+ * @b: 1024
+ *
+ * Return: 98*1024
+ */
+
+int mul(int a, int b)
+{
+
+	printf("98*1024\n");
+	scanf("%d %d", &a, &b);
+
+	printf("Multiplication of %d and %d is %d\n", a, b, (a * b));
+
+	return (0);
+}
