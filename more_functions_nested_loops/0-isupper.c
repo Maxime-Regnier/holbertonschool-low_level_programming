4795d2cb74838cbb58c4754c70bcf28a0ec66b40
Maxime-Regnier <11493@holbertonstudents.com>
2025-06-23 08:30:53 +0000

    file

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void) {
+
+	char ch = 'C';
+
+	if (isupper(ch)) {
+		printf("%c is an Uppercase Letter \n", ch);
+	}
+	else
+	{
+	printf("%c is not an Uppercase Letter \n", ch);
+	}
+	return 0;
+
+}
