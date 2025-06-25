34911009a3d6fffdc1cec4b86cf0afec90c9f54f
Maxime-Regnier <11493@holbertonstudents.com>
2025-06-25 09:08:44 +0000

    file

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print numbers from 0 to 9 followed by a newline
+ *
+ * not print 2
+ * and
+ * 4
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++);
+	{
+	_putchar(i + '0');
+	}
+	{
+	_putchar('\n');
+	}
+
+}
