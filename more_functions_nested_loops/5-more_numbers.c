892410edc99028a762321e5ee927960003c1166b
Maxime-Regnier <11493@holbertonstudents.com>
2025-06-26 15:04:44 +0000

    file

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,8 @@
 #include "main.h"
 
 /**
- * Task 5 - Numbers constitute the only universal language
- * 
- * Cette tâche consiste à écrire une fonction nommée more_numbers qui 
- * imprimera les nombres de 0 à 14, suivi d'un retour à la ligne, et cela, 
- * 10 fois consécutivement. 
- * 
- * Prototype : void more_numbers(void);
- * 
- * Contrainte : Vous ne pouvez utiliser la fonction _putchar que trois fois 
- * dans votre code, ce qui nécessite une approche réfléchie pour 
- * optimiser son utilisation tout en produisant la sortie requise.
- * 
- * Sortie attendue : La fonction doit afficher la séquence
- * "01234567891011121314" dix fois, chaque séquence sur une nouvelle ligne.
+ * more_numbers - Print 10 times the numbers since 0  up to 14
+ * Return 10 times of the numbers since 0 up to 14
  */
 
 void more_numbers(void)
@@ -23,7 +11,7 @@ void more_numbers(void)
 
 	for (x = 0; x <= 14; x++)
 	{
-	for (y= 0; y <= 14; y++)
+	for (y = 0; y <= 14; y++)
 	{
 	if (y > 9)
 	{
