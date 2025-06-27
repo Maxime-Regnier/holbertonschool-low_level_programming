de5dd2e1e0f17a0561d21c0f9da51a263a62e80f
Maxime-Regnier <11493@holbertonstudents.com>
2025-06-27 12:42:34 +0000

    file

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ */
+
+int main(void)
+
+{
+	int h;
+
+	for (h = 1; h <= 100; h++)
+	{
+	if ((h % 3 == 0) && (h % 5 == 0))
+	{
+	printf("FizzBuzz");
+	}
+	else if (h % 3 == 0)
+	{
+	printf("Fizz");
+	}
+	else if (h % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else
+	{
+	printf("%d", h);
+	}
+	if (h != 100)
+	{
+	printf(" ");
+	}
+	}
+	printf("\n");
+	return (0);
+}
