89ed1558fc82b233cc87cb7f1067299504c247af
Maxime-Regnier <11493@holbertonstudents.com>
2025-06-29 01:26:24 +0000

    file

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,45 +1,40 @@
 #include "main.h"
+#include <limits.h>
 
 /**
- * _atoi - takes a string and find numbers in it
- * @s: string of char with numbers in it (or not)
- (*
- * Description: Longer description of the function)?
-(* section header: Section description)*
- * Return: the number
+ * _atoi - convert string to int
+ * @s: string to convert
+ *
+ * Return: the string converted in integer
  */
 int _atoi(char *s)
 {
-	int i; /* compteur position 1er chiffre*/
-	int j; /* Compteur position chiffre de fin*/
-	int k;	/* boucle d'action entre les deux positions*/
-	int nbb = 0; /* variable accumulant les chiffres voulus*/
-	int l = 0; /* compteur de longueur de chaîne*/
-	int minus = 0; /*compteur de "-"*/
-	int mi = 0; /*compteur de boucle*/
+	int i = 0;
+	int tmp = 0;
+	int neg = 0;
 
-	while (s[l] != '\0')
+	while (s[i] != '\0')
 	{
-		++l;
+		if (s[i] == 45)
+			neg++;
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			if (tmp > (INT_MAX - s[i] + 48) / 10)
+			{
+				if (neg % 2 == 0)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
+			tmp = tmp * 10 + (s[i] - 48);
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
+		}
+		i++;
 	}
-	for (i = 0; (s[i] < '0' || s[i] > '9'); ++i)
-	{
-	}
-	for (j = i; (s[j] >= '0' && s[j] <= '9') && (j < l); ++j)
-	{
-	}
-	for (k = i; k < j; ++k)
-	{
-		nbb = nbb * 10 + (s[k] - 48);
-	}
-	for (mi = 0; (s[mi] < '0' || s[mi] > '9') && mi <= l; ++mi)
-	{
-		if (s[mi] == '-')
-			++minus;
-	}
-	if (minus % 2 == 1)
-	{
-		nbb = -nbb;
-	}
-	return (nbb);
+	if (tmp == 0)
+		return (0);
+	if (neg % 2 != 0)
+		tmp = -tmp;
+	return (tmp);
 }
