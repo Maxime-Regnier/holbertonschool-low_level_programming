efef9216bc209fbf557400fe6c88d3f185662e35
Maxime-Regnier <11493@holbertonstudents.com>
2025-06-29 00:48:04 +0000

    file

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints an aray a number of times
+ * @a: an array
+ * @n: the number of elements to print
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+	printf("\n");
+	return;
+	}
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", *(a + i));
+
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+
+	printf("\n");
+
+}
