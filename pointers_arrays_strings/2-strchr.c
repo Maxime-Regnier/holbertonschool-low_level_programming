c486e970ff639666d1a0780078205dfb007e980a
Maxime-Regnier <11493@holbertonstudents.com>
2025-07-01 13:49:10 +0000

    file

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,14 @@
 char *_strchr(char *s, char c)
 
 {
-	while (*s != '\0') {
-	if (*s == c) {
-	return s;
+	while (*s != '\0')
+	{
+	if (*s == c)
+	{
+	return (s);
 	}
 	s++;
 }
-return NULL;
+return (NULL);
 }
 
