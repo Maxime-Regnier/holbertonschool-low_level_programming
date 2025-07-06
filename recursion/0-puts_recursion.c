da399cc1f139b01a75d724f7fde1ed3967f77436
Maxime-Regnier <11493@holbertonstudents.com>
2025-07-06 08:51:09 +0000

    file

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Affiche une chaîne de caractères, suivie d'un saut de ligne.
- * @s: La chaîne de caractères à afficher.
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to print.
+ *
+ * This function prints the string pointed to by s, using recursion,
+ * followed by a new line. Similar to puts from the standard library.
  */
 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
-    _putchar(*s);
-    _puts_recursion(s + 1);
+	{
+	_putchar('\n');
+	return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
