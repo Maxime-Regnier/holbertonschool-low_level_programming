382b5c888c06312d54a60d205a9ed9d4e0e1e8dc
Maxime-Regnier <11493@holbertonstudents.com>
2025-07-08 10:36:32 +0000

    file

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,17 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
 
-/*
- *
- *
- *
- *
+/**
+ * create_array - Crée un tableau de taille `size` et l'initialise avec le caractère `c`
+ * @size: La taille du tableau
+ * @c: Le caractère avec lequel remplir le tableau
+ * 
+ * Return: Un pointeur vers le tableau créé, ou NULL en cas d'échec
  */
 
 char *create_array(unsigned int size, char c)
 
 {	
-	if size == (0)
-	return (NULL)
+	if (size == 0) {
+	return (NULL);
+	}
+char *array = malloc(sizeof(char) * (size + 1));
 
+if (array == NULL) {
+	return (NULL);
+for (unsigned int i = 0; i < size; i++)
+
+{
+array[i] = c;
+}
+array[size] = '\0';
+return array;
 }
