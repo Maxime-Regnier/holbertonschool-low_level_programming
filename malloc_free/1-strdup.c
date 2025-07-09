91887ccef10729fddbe181f52fbf4a69f6a3b07b
Maxime-Regnier <11493@holbertonstudents.com>
2025-07-09 08:27:08 +0000

    file

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/*
+ *
+ *
+ *
+ */
+
+char *_strdup(char *str)
+{
+char *strdup
+
+	if (strdup == NULL)
+	{
+	return (NULL);
+	}
+
+strdup = malloc(sizeof(char) * (size + 1));
+
+if (strdup == NULL)
+{
+	return (NULL); 
+}
+
+for (i = 0; i < size; i++)
+{
+	strdup[i] = c;
+}
+strdup[size] = '\0';
+
+return (strup);
+
+}
