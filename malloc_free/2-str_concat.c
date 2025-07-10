29cf90fd4ea6b9412be22a4df0764b3b668e79e4
Maxime-Regnier <11493@holbertonstudents.com>
2025-07-10 12:22:27 +0000

    file

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  *
@@ -9,7 +10,24 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *concat;
-	unsigned int i;
+        char *concat;
+        unsigned int len 1, len 2;
 
+if s1 == NULL
+        s1 = ""
+if s2 == NULL
+        s2 = ""
+len 1 = str_length(s1);
+len 2 = str_length(s2);
+
+concat = malloc(len1 + len2 + 1);
+
+if concat == NULL
+       return NULL;
+str_copy(concat, s1);
+str_copy(concat + len1, s2);
+
+concat[len1 + len2] = '\0';
+
+return concat;
 }
