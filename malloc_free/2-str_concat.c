62eed81be3f2cbc75f6fa1a283335e906371401c
Maxime-Regnier <11493@holbertonstudents.com>
2025-07-10 14:32:28 +0000

    file

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -2,32 +2,36 @@
 #include <stdlib.h>
 
 /**
+ * str_concat - concatène deux chaînes en une nouvel chaine
+ * @s1: pointeur sur la première chaîne raitee comme vide si NULL)
+ * @s2: pointeur sur la seconde chaîne traitee comme vide si NULL)
  *
+ * Alloue une zone mémoire pour contenir s1, s2 et le '\0'.
+ * L utilisateur doit libérer le résultat avec free().
  *
- *
- *
+ * Return: pointeur sur la chaîne concaténéeu NULL si malloc échoue.
  */
 
 char *str_concat(char *s1, char *s2)
 {
         char *concat;
-        unsigned int len 1, len 2;
+        unsigned int len1, len2;
 
-if s1 == NULL
-        s1 = ""
-if s2 == NULL
-        s2 = ""
-len 1 = str_length(s1);
-len 2 = str_length(s2);
+if (s1 == NULL)
+        s1 = "";
+if (s2 == NULL)
+        s2 = "";
+len1 = str_length(s1);
+len2 = str_length(s2);
 
 concat = malloc(len1 + len2 + 1);
 
-if concat == NULL
+if (concat == NULL)
        return NULL;
 str_copy(concat, s1);
 str_copy(concat + len1, s2);
 
 concat[len1 + len2] = '\0';
 
-return concat;
+return (concat);
 }
