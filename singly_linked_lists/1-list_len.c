930abd8f3ebff5d8080a85b1b0757988eda85c6d
Maxime-Regnier <11493@holbertonstudents.com>
2025-07-28 12:00:03 +0000

    file

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+size_t list_len(const list_t *h)
+{
+int fubdLength (struct Node* p
+	{
+		int count = 0;
+		while (p != NULL)
+		{
+		count++;
+		p = p->next;
+		}
+		return count;
+}
