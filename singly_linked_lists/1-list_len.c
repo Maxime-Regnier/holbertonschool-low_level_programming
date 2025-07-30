6475c28a518ecdf3fcc191f70180cd12278d21ed
Maxime-Regnier <11493@holbertonstudents.com>
2025-07-30 14:21:50 +0000

    file

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -10,6 +10,7 @@
 size_t list_len(const list_t *h)
 {
 		size_t count = 0;
+
 		while (h != NULL)
 		{
 		if (h->str == NULL)
@@ -19,5 +20,5 @@ size_t list_len(const list_t *h)
 		count++;
 		h = h->next;
 		}
-		return count;
+		return (count);
 }
