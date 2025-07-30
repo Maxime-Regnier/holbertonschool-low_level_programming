bd95eef35ba9ea1ab860b455ecabe8d3c6d1103d
Maxime-Regnier <11493@holbertonstudents.com>
2025-07-30 14:49:44 +0000

    file

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,17 +9,13 @@
  *
  * Description: singly linked list node structure
  */
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
 size_t print_list(const list_t *h)
 {
-
-
-
-
-
+	size_t count = 0;
+	while (h != NULL)
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else	printf("[%u] %s\n", h->len, h->str);
+	h = h->next;
+		return (count);
 }
