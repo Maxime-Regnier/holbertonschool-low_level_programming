e1cd2e064b4b6f2b2be632bb080bd307bdc4c6fd
Maxime-Regnier <11493@holbertonstudents.com>
2025-07-30 09:54:31 +0000

    file

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,23 +1,31 @@
 #include "lists.h"
+#include <stdio.h>
 
-/*
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- *
- *
- *
- *
- *
- *
+ * Description: singly linked list node structure
  */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 size_t list_len(const list_t *h)
 {
-int fubdLength (struct Node* p
-	{
-		int count = 0;
-		while (p != NULL)
+		size_t count = 0;
+		while (h != NULL)
 		{
+		if (h->str == NULL)
+		printf("[0] (nil)\n");
+		else
+		printf("[%u] %s\n", h->len, h->str);
 		count++;
-		p = p->next;
+		h = h->next;
 		}
 		return count;
 }
