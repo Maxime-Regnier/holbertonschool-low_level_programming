7810741cd1fe6dc92c35d3a461c48eb3f8ae9bfe
Maxime-Regnier <11493@holbertonstudents.com>
2025-07-30 15:06:18 +0000

    file

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -17,10 +17,8 @@ size_t print_list(const list_t *h)
 		printf("[0] (nil)\n");
 	else
 		printf("[%u] %s\n", h->len, h->str);
-	}
-	{
 	count++;
 	h = h->next;
-		return (count);
 	}
+		return (count);
 }
