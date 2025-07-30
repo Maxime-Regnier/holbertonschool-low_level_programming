248e587cd00d11df92139cd24430156fea35851e
Maxime-Regnier <11493@holbertonstudents.com>
2025-07-30 15:01:01 +0000

    file

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,20 +2,25 @@
 #include <stdio.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * print_list - Affiche tous les éléments d’une liste list_t
+ * @h: Pointeur vers la tête de la liste
  *
- * Description: singly linked list node structure
+ * Return: Nombre total de nœuds
  */
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
+
 	while (h != NULL)
-	if (h->str == NULL)
+	{
+		if (h->str == NULL)
 		printf("[0] (nil)\n");
-	else	printf("[%u] %s\n", h->len, h->str);
+	else
+		printf("[%u] %s\n", h->len, h->str);
+	}
+	{
+	count++;
 	h = h->next;
 		return (count);
+	}
 }
