0b33432f99b25c245bd804bdc2964e08d77d9a54
Maxime-Regnier <11493@holbertonstudents.com>
2025-07-31 14:35:06 +0000

    file

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -17,14 +17,28 @@ typedef struct list_s
 } list_t;
 
 /**
+ * add_node - Ajoute un nouveau nœud au début d'une liste list_t
+ * @head: Double pointeur vers le premier élément de la liste
+ * @str: Chaîne de caractères à dupliquer et stocker dans le nouveau nœud
  *
- *
- *
- *
+ * Return: Adresse du nouveau nœud, ou NULL si l'opération échoue
  */
 list_t *add_node(list_t **head, const char *str)
 {
-struct *add_node = malloc(sizeof(struct node));
+list_t *add_node = malloc(sizeof(liste_t));
+if (add_node == NULL)
+	return (NULL);
+add_node->str = strdup(str);
+if (add_node->str == NULL)
+{
+	free(add_node);
+	return (NULL);
+}
+add_node->len = strlen(str);
+add_node->next = *head;
+*head = add_node;
 
+return add_node;
+}
 
 }
