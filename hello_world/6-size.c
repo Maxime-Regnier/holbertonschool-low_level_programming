0fed9db5f75578f8528cffef61acea8e290166f2
Maxime-Regnier <11493@holbertonstudents.com>
2025-10-20 15:27:03 +0000

    Modified file 6-size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+int intType;
+float floatType;
+double doubleType;
+char charType;
 
+/**
+*sizeof evaluates the size of a variable
+*/
+printf("Size of int: %lu bytes\n", sizeof(intType));
+printf("Size of float: %lu bytes\n", sizeof(floatType));
+printf("Size of double: %lu bytes\n", sizeof(doubleType));
+printf("Size of char: %lu byte\n", sizeof(charType));
+return (0);
+}
