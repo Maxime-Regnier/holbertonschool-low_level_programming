72175b33bcbaa856f64a89f274429fd0df6b8e73
Maxime-Regnier <11493@holbertonstudents.com>
2025-10-21 08:07:54 +0000

    Modified file 6-size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
-int main(void)
-{
+
 /**
  * main - Displays the size of basic C data types
  * using the sizeof operator. Prints int, float, double,
@@ -8,11 +7,12 @@ int main(void)
  * Return: Always 0 (Success)
  */
 
-
+int main(void)
+{
 printf("Size of a char: %lu byte\n", sizeof(char));
 printf("Size of an int: %lu bytes\n", sizeof(int));
 printf("Size of a long int: %lu bytes\n", sizeof(long int));
-printf("Size of long long int: %lu bytes\n", sizeof(long long int));
+printf("Size of a long long int: %lu bytes\n", sizeof(long long int));
 printf("Size of a float: %lu bytes\n", sizeof(float));
 return (0);
 }
