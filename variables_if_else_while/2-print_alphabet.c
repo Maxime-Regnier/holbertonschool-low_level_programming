5c6051b931621a016cc836671fddc1f8265bc9b3
Maxime-Regnier <11493@holbertonstudents.com>
2025-10-21 22:27:28 +0000

    2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char g;
+
+	g = 97;
+	while (g >= 'a' && g <= 'z')
+	{
+		putchar(g);
+		g++;
+	}
+	putchar(10);
+	return (0);
+}
