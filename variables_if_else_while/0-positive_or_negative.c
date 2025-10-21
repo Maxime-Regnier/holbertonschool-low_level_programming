9a4357a8e8d5bcb3bb2c7a37f5aed7d882ece08e
Maxime-Regnier <11493@holbertonstudents.com>
2025-10-21 10:55:29 +0000

    Modified file 0-positive

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -19,19 +19,19 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-printf("int\n");
+printf("%d\n");
 
 if (n > 0)
 {
-printf("int is positive\n");
+printf("%d is positive\n");
 }
 else if (n < 0)
 {
-printf("int is negative\n");
+printf("%d is negative\n");
 }
 else
 {
-printf("int is zero\n");
+printf("%d is zero\n");
 }
 return (0);
 }
