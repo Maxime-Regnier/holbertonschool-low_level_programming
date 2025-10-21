34093a5341e4d694c8f2c7eabba05cdcd1b3702d
Maxime-Regnier <11493@holbertonstudents.com>
2025-10-21 08:01:25 +0000

    Modified file 6-size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,9 +9,10 @@
 
 int main(void)
 {
-printf("Size of int: %lu bytes\n", sizeof(int));
-printf("Size of float: %lu bytes\n", sizeof(float));
-printf("Size of double: %lu bytes\n", sizeof(double));
-printf("Size of char: %lu byte\n", sizeof(char));
+printf("Size of an int: %lu bytes\n", sizeof(int));
+printf("Size of a float: %lu bytes\n", sizeof(float));
+printf("Size of long long int: %lu bytes\n", sizeof(long long int));
+printf("Size of a long int: %lu bytes\n", sizeof(long int));
+printf("Size of a char: %lu byte\n", sizeof(char));
 return (0);
 }
