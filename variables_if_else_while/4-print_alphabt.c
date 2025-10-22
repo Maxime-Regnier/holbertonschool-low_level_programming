da6ad4a55d15a0862bec3401a97a7099a4393199
Maxime-Regnier <11493@holbertonstudents.com>
2025-10-22 12:38:47 +0200

    Modified file 4-print

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,13 +10,10 @@ int main(void)
 char r;
 r = 97;
 while (r >= 'a' && r <= 'z')
+if (r != 'e' && r != 'q')
 {
 putchar(r);
 r++;
-while (r > 113 && r < 101)
-{
-r++;
-}
 }
 putchar(10);
 return (0);
