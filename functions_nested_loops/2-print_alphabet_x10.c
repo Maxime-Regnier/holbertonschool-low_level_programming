f54e7f3711fe9421e6cca1bf8b224c4df1da186e
Maxime-Regnier <11493@holbertonstudents.com>
2025-10-23 18:44:56 +0200

    Modified file

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+char z;
+z = 'a';
+while (z >= 'a' && z <= 'z')
+{
+_putchar(z);
+z++;
+}
+_putchar('\n');
+}
