189199d91b05cf19d06635a244f82d35653cc3ee
Maxime-Regnier <11493@holbertonstudents.com>
2025-10-23 17:16:02 +0200

    Modified file

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-    char j[] = "_putchar"; 
-    int k = 0;
-        while (k < 8)
-    {
-        _putchar(j[k]);
-        k++;
-    }
-    _putchar('\n');
-    return (0);
+char j[] = "_putchar";
+int k = 0;
+while (k < 8)
+{
+_putchar(j[k]);
+k++;
+}
+_putchar('\n');
+return (0);
 }
