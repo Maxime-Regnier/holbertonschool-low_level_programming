a0872b0951d8c4922d95275e02b61860907f5cc1
Maxime-Regnier <11493@holbertonstudents.com>
2025-10-25 22:43:25 +0200

    Modified file

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * 
- * 
- * 
- * 
+ * add - Entry point
+ *
+ * @t: premier nb
+ * @b: second nb
+ *
+ * Return: (t + b)
  */
-int add(int, int)
+int add(int t, int b)
 {
-
-
-
+return (t + b);
 }
