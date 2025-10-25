b86830fa1a72fe66b4b76112363f18f487da4fcb
Maxime-Regnier <11493@holbertonstudents.com>
2025-10-25 23:45:44 +0200

    Modified file

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,26 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * 
- * 
- * 
- * 
- */
+*print_to_98 - - Entry point
+*@n: valeur
+*
+*Écrivez une fonction qui imprime tous les nombres naturels de n à 98,
+*suivis d'une nouvelle ligne.
+*
+* Return: Always 0
+*/
 void print_to_98(int n)
+
 {
-
-
+if (n >= 98)
+while (n > 98)
+{
+printf("%d, ", n--);
+}
+else
+while (n < 98)
+{
+printf("%d, ", n++);
+}
+printf("%d\n", n);
 }
