0d9f03f713b672afe092dac720783065f1f820b3
Maxime-Regnier <11493@holbertonstudents.com>
2025-10-27 14:21:00 +0100

    Modified file 3-print

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
- * 
- * 
- * 
+ * print_numbers - Prints the numbers from 0 to 9
+ *
+ * Description: This function uses a loop to print all digits
+ * from 0 to 9, followed by a new line. It uses the _putchar
+ * function to display each character.
+ *
+ * Return: Nothing (void)
  */
 void print_numbers(void)
 {
-int p = 0;
-for (p = 48; p < 58; p++)
+char p = 0;
+for (p = '0'; p <= '9'; p++)
 {
 _putchar(p);
 }
