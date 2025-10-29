33be93b143c2f13b8bc170ebe3d01ea5eb3f413f
Maxime-Regnier <11493@holbertonstudents.com>
2025-10-29 16:50:45 +0100

    Modified file strlen

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,12 @@
 #include "main.h"
 /**
- * 
- * 
- * 
+ * _strlen - Calcule la longueur d'une chaîne de caractères
+ * @s: La chaîne dont on veut connaître la longueur
+ *
+ * Description: Cette fonction parcourt chaque caractère de la chaîne
+ * jusqu'au caractère nul '\0' et retourne le nombre de caractères.
+ *
+ * Return: La longueur de la chaîne
  */
 int _strlen(char *s)
 {
