b2c6f43c7488e7f0f564b6761b02418c80b370ab
Maxime-Regnier <11493@holbertonstudents.com>
2025-10-29 15:11:39 +0100

    Modified file 0-reset

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * 
- * 
- * 
+ * reset_to_98 - Modifie la valeur pointée par un pointeur vers 98
+ * @n: Pointeur vers un entier à mettre à jour
  */
 void reset_to_98(int *n)
 {
