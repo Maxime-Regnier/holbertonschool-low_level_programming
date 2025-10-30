20d889662ac1d05f25c8c6edc70a3b42df3e51ed
Maxime-Regnier <11493@holbertonstudents.com>
2025-10-30 16:48:25 +0100

    Modified file 10-print

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,10 +1,13 @@
 #include "main.h"
 /**
- *
- *
- *
- *
- */
+* print_triangle - prints a triangle using the character '#'
+* @size: size of the triangle
+*
+* Description: This function prints a right-aligned triangle of '#' characters.
+* Each row contains spaces followed by '#' characters. The number of rows is
+* equal to the value of 'size'. If 'size' is 0 or negative, only a newline is printed.
+* Only the _putchar function is used to print characters.
+*/
 void print_triangle(int size)
 {
 int b;
