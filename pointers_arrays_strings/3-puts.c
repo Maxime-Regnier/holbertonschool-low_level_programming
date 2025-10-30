45037769f5fbfc88dd626a21f3e58c11c9afbf41
Maxime-Regnier <11493@holbertonstudents.com>
2025-10-30 17:00:54 +0100

    Modified file 3-puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- * 
- * 
- * 
- * 
- */
+* _puts - prints a string followed by a new line
+* @str: pointer to the string to print
+*
+* Description: This function prints each character of the string pointed to by
+* 'str' using the _putchar function. After printing the string, it prints a
+* newline character. Only _putchar is used to print characters.
+*/
 void _puts(char *str)
 {
-    char *x= str;
+char *x = str;
 while (*x)
 {
 _putchar(*x);
