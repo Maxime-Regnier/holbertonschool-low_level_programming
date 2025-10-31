36e771b3ede26f2e66205b3e16e795d06e06a289
Maxime-Regnier <11493@holbertonstudents.com>
2025-10-31 16:27:27 +0100

    Modified file 5-rev

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,8 +1,12 @@
 #include "main.h"
 /**
- * 
- * 
- * 
+ * rev_string - reverse a string in place
+ * @s: pointer to the string to reverse
+ *
+ * This function takes a pointer to a string and reverses
+ * the order of its characters directly in memory.
+ * It uses two pointers that start at the beginning and end
+ * of the string and swaps their values until they meet.
  */
 void rev_string(char *s)
 {
@@ -14,10 +18,10 @@ m++;
 m--;
 while (l < m)
 {
-    b = *l;
-    *l = *m;
-    *m = b;
-    l++;
-    m--;
+b = *l;
+*l = *m;
+*m = b;
+l++;
+m--;
 }
 }
