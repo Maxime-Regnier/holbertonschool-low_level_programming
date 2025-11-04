a08fdfe72af039940a519f0c1b9939c6853a2b80
Maxime-Regnier <11493@holbertonstudents.com>
2025-11-04 15:28:27 +0100

    Modified file

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,21 @@
 #include "main.h"
 /**
- * 
- * 
- * 
- */
+* print_chessboard - prints the chessboard
+* @a: pointer to 2D array of 8 elements
+*
+* Return: void
+*/
 void print_chessboard(char (*a)[8])
 {
+int e;
+int l;
 
+for (e = 0; e < 8; e++)
+{
+for (l = 0; l < 8; l++)
+{
+_putchar(a[e][l]);
+}
+_putchar('\n');
+}
 }
