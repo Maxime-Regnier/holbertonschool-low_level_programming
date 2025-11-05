057bf51a2ab818523995ab09066e60fc49217042
Maxime-Regnier <11493@holbertonstudents.com>
2025-11-05 10:51:59 +0100

    Modified file 0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * 
+ * 
+ * 
+ */
+int main(void)
+{
+    char name[100];
+printf("Enter Your Name: ");
+scanf("%s", name);
+printf("Your Name: %s\n", name);
+return (0);
+}
