4f731f598c0c2dd6f74858be84e7cba7d2b74d33
Maxime-Regnier <11493@holbertonstudents.com>
2025-11-05 12:19:01 +0100

    Modified file 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 /**
- * 
- * 
- * 
- */
-int main(void)
+* main - Entry point
+* Description: Program that prints its name, followed by a new line.
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: Always 0 (Success)
+*/
+int main (int argc, char *argv[])
 {
-printf("mynameis \n");
+    (void)argc;
+printf("%s\n", argv[0]);
 return (0);
 }
