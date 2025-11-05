4fc812be9777ea6c4f31911ab26727f6bac33fb3
Maxime-Regnier <11493@holbertonstudents.com>
2025-11-05 16:17:21 +0100

    Modified file 1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 /**
-* Programme qui affiche le nombre d'arguments passés au programme.
+* main - Prints the number of arguments passed to the program
+* @argc: Number of arguments passed to the program
+* @argv: Array of pointers to the arguments
 *
+* Description: The program prints the number of arguments passed
+* to it, excluding the name of the program itself. Each argument
+* is counted, and the total number is printed followed by a new line.
 *
+* Return: 0 on success
 */
 int main(int argc, char *argv[])
 {
