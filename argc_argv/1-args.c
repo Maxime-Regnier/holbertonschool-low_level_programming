b70b3ff5c8624aeb7208e575f8c59bf1d46f4622
Maxime-Regnier <11493@holbertonstudents.com>
2025-11-05 16:12:07 +0100

    Modified file 1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* Programme qui affiche le nombre d'arguments passés au programme.
+*
+*/
+int main(int argc, char *argv[])
+{
+int l = argc - 1;
+(void)argv;
+printf("%d\n", l);
+return (0);
+}
