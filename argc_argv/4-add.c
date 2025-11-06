33903a790e7034a39a12943d59a89102b70365dc
Maxime-Regnier <11493@holbertonstudents.com>
2025-11-06 16:50:26 +0100

    Modified file 4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,1 +1,34 @@
-#include
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Adds positive numbers
+ * @argc: Number of arguments
+ * @argv: Array of argument strings
+ *
+ * Return: 0 if success, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+int b = 0;
+int s, r;
+
+if (argc == 1)
+{
+printf("%d\n", 0);
+return (0);
+}
+for (s = 1; s < argc; s++)
+{
+for (r = 0; argv[s][r] != '\0'; r++)
+{
+if (argv[s][r] < '0' || argv[s][r] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+b += atoi(argv[s]);
+}
+printf("%d\n", b);
+return (0);
+}
