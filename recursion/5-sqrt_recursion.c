ca426b65dccec564c5ad5c5facf7e33aeae4b104
Maxime-Regnier <11493@holbertonstudents.com>
2025-11-09 23:36:03 +0100

    Modified file

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,4 +1,26 @@
 #include "main.h"
+
+/**
+* _sqrt_helper - Recursively finds the natural square root of a number
+* @n: The number to find the square root of
+* @candidate: The current number to test
+*
+* Return: A. candidate  B. -1)
+*/
+
+int _sqrt_helper(int n, int candidate)
+{
+if (candidate * candidate == n)
+{
+return candidate;
+}
+if (candidate * candidate > n)
+{
+return -1;
+} 
+return _sqrt_helper(n, candidate + 1);
+}
+
 /**
 *  _sqrt_recursion - Returns the natural square root of a number
 * @n: The number to find the square root of
@@ -11,9 +33,5 @@ if (n < 0)
 {
 return (-1);
 }
-if (n * n > n)
-{
-return (n);
-}
-return (_sqrt_recursion(n + 1, n));
+return _sqrt_helper(n, 0);
 }
