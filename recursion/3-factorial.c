b6a5ae957779fff60a52e3bdf49ab323f3948fa4
Maxime-Regnier <11493@holbertonstudents.com>
2025-11-09 17:20:09 +0100

    Modified file 3-factorial

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -2,7 +2,7 @@
 /**
 * factorial - Returns the factorial of a given number using recursion
 * @n: integer to calculate factorial
-* 
+*
 * Return: factorial result
 */
 int factorial(int n)
@@ -13,7 +13,7 @@ return (-1);
 }
 if (n == 0)
 {
-return (0);
+return (1);
 }
-return (n + factorial(n - 1));
+return (n * factorial(n - 1));
 }
