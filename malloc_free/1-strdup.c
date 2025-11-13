3b61e2ad6e5158d5353ef28b457561b6555ac65a
Maxime-Regnier <11493@holbertonstudents.com>
2025-11-13 14:01:03 +0100

    Modified file 1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+/**
+* _strdup - ________________________________
+* @str: ________________________________
+* Return: ________________________________
+*/
+char *_strdup(char *str)
+{
+char *dup;
+unsigned int r, len = 0;
+if (str == NULL)
+return (NULL);
+while (str[len] != '\0')
+len++;
+dup = malloc(len);
+if (dup == NULL)
+return (NULL);
+for (r = 0; r < len; r++)
+dup[r] = str[r];
+dup[r] = '\0';
+return (dup);
+}
