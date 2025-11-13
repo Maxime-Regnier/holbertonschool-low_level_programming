0932c5e247739fe842927e1879d5df1e9fd952ef
Maxime-Regnier <11493@holbertonstudents.com>
2025-11-13 14:07:54 +0100

    Modified file 1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,7 +12,7 @@ if (str == NULL)
 return (NULL);
 while (str[len] != '\0')
 len++;
-dup = malloc(len);
+dup = malloc((len + 1) * sizeof(char));
 if (dup == NULL)
 return (NULL);
 for (r = 0; r < len; r++)
