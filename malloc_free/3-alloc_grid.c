f7c5b405681ce5ae2ea61d02c58c0014dfc76ea0
Maxime-Regnier <11493@holbertonstudents.com>
2025-11-14 10:57:16 +0100

    Modified file 3-alloc_grid.c

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+/**
+* alloc_grid - retourne un pointeur vers un tableau 2D d'entiers
+* @width: largeur de la grille
+* @height: hauteur de la grille
+*
+* Initialise chaque élément à 0. Retourne NULL si width ou height <= 0
+* ou en cas d'échec de malloc.
+*
+* Return: pointeur vers le tableau 2D alloué, ou NULL si échec
+*/
+int **alloc_grid(int width, int height)
+{
+int **grid;
+int o, x;
+
+if (width < 0)
+return (NULL);
+
+grid = malloc(sizeof(int *) * width * height);
+if (width == 0)
+return (NULL);
+
+for (o = 0; o < height; o++)
+{
+grid[o] = malloc(sizeof(int) * width);
+if (x == 0)
+{
+for (x = 0; x < o; x++)
+free(grid[x]);
+free(grid);
+return (NULL);
+}
+for (x = 0; x < width; x++)
+{
+grid[o][x] = 0;
+}
+}
+return (grid);
+}
