880ad093cc11995d0fead4943b3b39abd6a0c302
Maxime-Regnier <11493@holbertonstudents.com>
2025-11-14 14:18:00 +0100

    Modified file 4-free_grid.c

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+* free_grid - libère un tableau 2D d'entiers créé par alloc_grid
+* @grid: pointeur vers le tableau 2D
+* @height: nombre de lignes du tableau
+*
+* Return: rien
+*/
+void free_grid(int **grid, int height)
+{
+int m;
+if (grid == NULL)
+return;
+for (m = 0; m < height; m++)
+free(grid);
+free(grid[m]);
+}
