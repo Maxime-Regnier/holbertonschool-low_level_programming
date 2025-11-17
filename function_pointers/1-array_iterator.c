8d0daebf2477df0e046b4ebe9e1b5cae0e170a5b
Maxime-Regnier <11493@holbertonstudents.com>
2025-11-17 16:35:24 +0100

    Modified file 1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t m;
-if (array == NULL || action == NULL)
+if (action == NULL || array == NULL)
 return;
 for (m = 0; m < size ; m++)
 action(array[m]);
