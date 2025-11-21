e9ea8fb8909bc23c6d6d16601e81afd80fec3f59
Maxime-Regnier <11493@holbertonstudents.com>
2025-11-21 21:09:02 +0100

    Modified file description doc betty 2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,10 +1,15 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- * 
- * 
- * 
- */
+* print_strings - Affiche des chaînes de caractères
+* @separator: La chaîne à afficher entre chaque chaîne passée en argument
+* @n: Le nombre de chaînes passées à la fonction
+*
+* Description: Cette fonction affiche toutes les chaînes passées en arguments
+* séparées par la chaîne @separator Si @separator est NULL aucun séparateur
+* n'est affiché. Si une des chaînes est NULL "(nil)" est affiché à la place
+* Après l'affichage de toutes les chaînes, un saut de ligne est imprimé.
+*/
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 va_list args;
