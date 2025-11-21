ad010bb73807566f65f8295d4b1f7ddb7702caf4
Maxime-Regnier <11493@holbertonstudents.com>
2025-11-21 21:54:22 +0100

    Description code file 0-sum_them_all.c + suppression d'espace pour betty

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,9 +1,13 @@
 #include <stdarg.h>
 /**
- * 
- * 
- * 
- */
+* sum_them_all - Returns the sum of all integers passed as arguments
+* @n: Number of arguments to sum
+*
+* Description: Takes a variable number of integers and returns their sum.
+* If n is 0, returns 0. Uses va_list, va_start, va_arg, and va_end macros.
+*
+* Return: Sum of all integers passed as parameters.
+*/
 int sum_them_all(const unsigned int n, ...)
 {
 va_list args;
@@ -11,16 +15,16 @@ int sum;
 unsigned int i;
 
 if (n == 0)
-return 0;
+return (0);
 sum = 0;
-va_start (args, n);
+va_start(args, n);
 
 for (i = 0; i < n; i++)
-    {
-        sum += va_arg (args, int);
-    }
+{
+sum += va_arg(args, int);
+}
 
-    va_end (args);
+va_end(args);
 
-    return sum;
+return (sum);
 }
