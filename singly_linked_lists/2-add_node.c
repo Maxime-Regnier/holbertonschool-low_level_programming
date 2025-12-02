8c539fcc2df03d6563bce6c9c5839680c4ffc015
Maxime-Regnier <11493@holbertonstudents.com>
2025-12-02 16:59:04 +0100

    "Add add_node function with Betty description and compiler file"

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,10 +3,16 @@
 #include <string.h>
 #include "lists.h"
 /**
- * 
- * 
- * 
- */
+* add_node - Adds a new node at the beginning of a linked list
+* @head: Double pointer to the head of the list
+* @str: String to store in the new node
+*
+* Description: Creates a new node containing a copy of `str`.
+* Inserts it at the beginning of the list. Returns the new node.
+* Returns NULL if memory allocation fails.
+*
+* Return: Pointer to the new node, or NULL if allocation fails
+*/
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *add_node = malloc(sizeof(list_t));
