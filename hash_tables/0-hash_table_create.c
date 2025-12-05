0c34643b537543dbeeb623a13f06edf5f8a348dc
Maxime-Regnier <11493@holbertonstudents.com>
2025-12-05 21:53:19 +0100

    "Fix indentation in 0-hash_table_create.c and hash_tables.h to comply with Betty style"

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,11 +2,11 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array
- *
- * Return: pointer to new table, or NULL
- */
+* hash_table_create - creates a hash table
+* @size: size of the array
+*
+* Return: pointer to new table, or NULL
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *kb;
