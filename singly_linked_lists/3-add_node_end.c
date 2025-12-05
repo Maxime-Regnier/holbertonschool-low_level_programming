8e376c1976c72bff01a612c3ef7ec0df28fa3bf4
Maxime-Regnier <11493@holbertonstudents.com>
2025-12-05 09:56:35 +0100

    "Add function to append a node at the end of a list"

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *new;
+list_t *temp;
+unsigned int i = 0;
+
+new = malloc(sizeof(list_t));
+if (new == NULL)
+return (NULL);
+
+new->str = strdup(str);
+if (new->str == NULL)
+{
+free(new);
+return (NULL);
+}
+
+while (new->str[i] != '\0')
+i++;
+new->len = i;
+if (*head == NULL)
+{
+*head = new;
+}
+else
+{
+temp = *head;
+while (temp->next != NULL)
+temp = temp->next;
+temp->next = new;
+}
+return (new);
+}
