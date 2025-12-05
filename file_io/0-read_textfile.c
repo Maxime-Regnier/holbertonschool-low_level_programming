52d42e86cf9c7f5696cb06cbb3a314b64a4e0c60
Maxime-Regnier <11493@holbertonstudents.com>
2025-12-05 11:35:02 +0100

    "Add read_textfile function to print a file to POSIX stdout"

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+/**
+ * 
+ * 
+ * 
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd;
+char *buffer;
+ssize_t read_bytes;
+ssize_t write_bytes;
+if (filename == NULL)
+{
+return (0);
+}
+fd = open(filename, O_RDONLY);
+if (fd < 0)
+{
+return (0);
+}
+buffer = malloc(letters * sizeof(char));
+read_bytes = read(fd, buffer, letters);
+if (read_bytes == -1)
+{
+free(buffer);
+return (0);
+}
+write_bytes = write(1, buffer, read_bytes);
+if (write_bytes < read_bytes)
+{
+free(buffer);
+return (0);
+}
+free(buffer);
+close(fd);
+return (write_bytes);
+}
