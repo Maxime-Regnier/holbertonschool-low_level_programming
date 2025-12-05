fd9eb0114fdcfe7d3236ee0077835b69a6ac33ad
Maxime-Regnier <11493@holbertonstudents.com>
2025-12-05 10:00:39 +0100

    "Add Betty-compliant header comment for add_node_end function"

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,11 +3,17 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * 
- * 
- * 
- * 
- */
+  * add_node_end - Adds a new node at the end of a list_t list
+  * @head: A pointer to the pointer to the head of the list
+  * @str: The string to be duplicated and stored in the new node
+  *
+  * Description: This function allocates memory for a new node, duplicates
+  * the string str, calculates its length, sets the next pointer to NULL,
+  * and appends the node at the end of the list. If the list is empty,
+  * the new node becomes the head. If memory allocation fails, returns NULL.
+  *
+  * Return: The address of the new element, or NULL if it failed
+  */
 list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *new;
