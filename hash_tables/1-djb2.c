74a3b7330c1f35f4a8398cfbb1c8ac6a63c29f74
Maxime-Regnier <11493@holbertonstudents.com>
2026-01-09 11:37:23 +0100

    Add hash_djb2 function with Betty-compliant comments

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,10 +1,15 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 /**
- * 
- * 
- * 
- * 
+ * hash_djb2 - Hash function implementing the DJB2 algorithm
+ * @str: The string to hash
+ *
+ * Description: This function takes a string and computes its hash
+ * value using the DJB2 algorithm by Dan Bernstein. It shifts the
+ * current hash left by 5 bits, adds the previous hash, then adds
+ * the current character, iterating over all characters in the string.
+ *
+ * Return: The computed hash as an unsigned long int
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
